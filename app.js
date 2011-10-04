31bbb7c99ee5ddff7abdde53e13e5dc6b568536c
Glen Mailer <glen.mailer@bskyb.com>
2011-10-04 09:33:07 +0100

    Leave the previous implementation in the file for reference

--- app.js
+++ app.js
@@ -20,6 +20,27 @@ var fibonacci = function(n, callback) {
     }
 }
 
+/*** For reference, and to save you looking at the revision history,
+ *   this is the previous implementation.
+var fibonacci = function(n, callback) {
+    if (n <= 2) {
+        callback(null, 1);
+        return;
+    }
+    async.series({
+        n2: function(next) {
+            process.nextTick(function() { fibonacci(n - 2, next); });
+        },
+        n1: function(next) {
+            process.nextTick(function() { fibonacci(n - 1, next); });
+        },
+    }, function(err, results) {
+        callback(null, results.n1 + results.n2);
+    });
+}
+fibonacci = async.memoize(fibonacci);
+ */
+
 app.get('/:n', function(req, res) {
     fibonacci(req.params.n, function(err, number) {
         res.send(''+number);
